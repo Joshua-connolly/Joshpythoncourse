496c31684951ae9c95f597dcb48dbd9e2a4ed57a
Joshua-connolly <joshua_connolly@ymail.com>
2021-06-06 17:35:35 +0100

    Create R Studio.R

---
+++ R Studio.R
@@ -0,0 +1,19 @@
+name = readline(prompt="Input your name: ")
+age =  readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
+
+ls()
+20:50 
+
+x <-  20:60 
+
+result.mean <-mean(x)
+print(result.mean)
+
+y <-  51:91
+
+result.sum <-sum(y)
+print(result.sum)
+
+z <- runif(n = 10, min = -50, max = 50)
+print(z)
